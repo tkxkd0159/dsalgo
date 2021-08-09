8124a48bdad86846fd9a9edfb7a330d5158a1c25
Jaeseung Lee <41176085+tkxkd0159@users.noreply.github.com>
2021-08-09 21:08:52 +0900

    Create leet20.py

---
+++ PS/leet20.py
@@ -0,0 +1,45 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        l = len(s)
+        if l % 2 != 0:
+            return False
+        if s is None or l <= 1:
+            return False
+        left_ = "({["
+        right_ = ")}]"
+        set_ = ["[]", "{}", "()"]
+        map_ = {"{":"}", "(":")", "[":"]"}
+        ord_q = []
+        skip_ = 0
+
+        for i in range(l):
+            if skip_ != 0:
+                skip_ -= 1
+                continue
+            if s[i:i+2] not in set_ and s[i] in left_ and (i+1) < l:
+                if s[i+1] in right_:
+                    return False
+                ord_q.append(s[i])
+                continue
+
+            elif s[i:i+2] in set_:
+                skip_ = 1
+                continue
+
+            elif s[i] in right_:
+                if ord_q == []:
+                    return False
+                tmp = ord_q.pop()
+
+                if map_[tmp] == s[i]:
+                    continue
+                else:
+                    return False
+
+            else:
+                False
+
+        if ord_q == []:
+            return True
+        else:
+            return False
