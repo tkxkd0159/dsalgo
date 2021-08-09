679ec635b3f98d119b0378083050759dd50c5ac4
Jaeseung Lee <41176085+tkxkd0159@users.noreply.github.com>
2021-08-10 00:08:08 +0900

    Update leet05.py

--- PS/leet05.py
+++ PS/leet05.py
@@ -21,3 +21,20 @@ class Solution:
                 start = i - (final_len - 1) // 2
                 end = i + final_len // 2
         return s[start:end+1]
+
+    # DP, slower than first method
+    def longestPalindrome2(self, s: str) -> str:    
+        n = len(s)
+        res = ""
+        if n < 1 or s is None:
+            return ""
+        
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+        for i in range(n-1, -1, -1):
+            for j in range(i, n, 1):
+                matrix[i][j] = (s[i] == s[j]) and (j - i < 3 or matrix[i+1][j-1])
+                
+                if matrix[i][j] and (j+1-i) > len(res):
+                    res = s[i:j+1]
+              
+        return res
