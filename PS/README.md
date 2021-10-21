953b8741d05a0ebd0f2bc4101f47a96578542cac
Jaeseung Lee(KU) <41176085+tkxkd0159@users.noreply.github.com>
2021-10-21 12:41:16 +0900

    Reorganize PS directory

--- PS/README.md
+++ PS/README.md
@@ -1,5 +1,17 @@
 # Need to review
 
 # Baekjoon
-## Backtracking
+### Backtracking
 `#15649`
+
+# Programmers
+### Hash
+kit : `1`, `2`
+### Stack & Queue
+kit : `1`
+
+# Leetcode
+### Stack & Queue
+`5`
+### DP
+`20`
--- PS/baekjoon/15649.py
+++ PS/baekjoon/15649.py
@@ -1,19 +1,22 @@
 # N과 M
 n, m = map(int, input().split())
-visited = [0 for _ in range(n+1)]
+visited = [0 for _ in range(n + 1)]
 seq_arr = [0 for _ in range(m)]
 
+
 def dfs(cnt):
-    if (cnt == m):
+    if cnt == m:
         for elem in seq_arr:
             print(elem, end=' ')
         print("")
         return 0
 
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         if visited[i] is not True:
             visited[i] = True
             seq_arr[cnt] = i
-            dfs(cnt+1)
+            dfs(cnt + 1)
             visited[i] = False
-dfs(0)
+
+
+dfs(0)  # 4 2
