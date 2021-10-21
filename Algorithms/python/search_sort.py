5e505ec3ac97a5bb65dbf3adaf6552ccf2a5f19a
Jaeseung Lee(KU) <41176085+tkxkd0159@users.noreply.github.com>
2021-10-21 19:39:48 +0900

    Add fine_element

--- Algorithms/python/search_sort.py
+++ Algorithms/python/search_sort.py
@@ -27,6 +27,7 @@ class Search:
         return None
 
 
+
 class Sort:
     """The Class of Sorts
     """
@@ -162,12 +163,34 @@ def score_top_down():
     for name, _ in arr:
         print(f'{name}', end=" ")
 
+def find_element():
+    def bin_search(arr, target, start, end):
+        while start <= end:
+            mid = (start + end) // 2
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] < target:
+                start = mid + 1
+            elif arr[mid] > target:
+                end = mid - 1
+        return None
+
+    n, m = 5, 3  # 매장의 부품 개수 / 손님이 요청한 부품 개수
+    elem_ls = [8, 3, 7, 9, 2]
+    elem_ls.sort()
+    client_elem = [5, 7, 9]
+    for elem in client_elem:
+        if bin_search(elem_ls, elem, 0, len(elem_ls)-1):
+            print("yes", end=" ")
+        else:
+            print("no", end=" ")
 
 BOOK = True
 
 if __name__ == "__main__":
     if BOOK:
         # top_down()
-        score_top_down()
+        # score_top_down()
+        find_element()
     else:
         main()
