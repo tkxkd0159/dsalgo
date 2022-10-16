1a31021062bba14c2dc31c78c27949188528d63e
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2022-10-16 23:52:30 +0900

    solve deque ps2

--- PS/baekjoon/base/easy.py
+++ PS/baekjoon/base/easy.py
@@ -1,4 +1,7 @@
 from collections import deque
+import sys
+read = sys.stdin.readline
+
 
 def solve2164():
     N = int(input())
@@ -7,3 +10,41 @@ def solve2164():
         cards.popleft()
         cards.append(cards.popleft())
     print(cards.pop())
+
+def solve10866():
+    N = int(read())
+    dq = deque()
+    for _ in range(N):
+        target = read().split()
+
+        if target[0] == "push_front":
+            dq.appendleft(int(target[1]))
+        elif target[0] == "push_back":
+            dq.append(int(target[1]))
+        elif target[0] == "pop_front":
+            if len(dq) == 0:
+                print(-1)
+            else:
+                print(dq.popleft())
+        elif target[0] == "pop_back":
+            if len(dq) == 0:
+                print(-1)
+            else:
+                print(dq.pop())
+        elif target[0] == "size":
+            print(len(dq))
+        elif target[0] == "empty":
+            if len(dq) == 0:
+                print(1)
+            else:
+                print(0)
+        elif target[0] == "front":
+            if len(dq) == 0:
+                print(-1)
+            else:
+                print(dq[0])
+        elif target[0] == "back":
+            if len(dq) == 0:
+                print(-1)
+            else:
+                print(dq[-1])
